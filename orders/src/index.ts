e4a2efd93716b95cf2217ed3710700319eb84432
michael-rayner92 <39574445+michael-rayner92@users.noreply.github.com>
2023-03-29 15:13:43 +0200

    added start up message to orders srv

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,8 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 const PORT = 3000;
 
 const start = async () => {
+	console.log('Starting up orders service...');
+
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
